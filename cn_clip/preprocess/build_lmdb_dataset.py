abe24e9a512d59bd882a72e36af11bbd0e14e0ab
zhenhua32 <zhenhua32@gmail.com>
2023-05-18 21:25:33 +0800

    写点注释, 看了下数据构建的脚本

--- cn_clip/preprocess/build_lmdb_dataset.py
+++ cn_clip/preprocess/build_lmdb_dataset.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 This script serializes images and image-text pair annotations into LMDB files,
 which supports more convenient dataset loading and random access to samples during training 
 compared with TSV and Jsonl data files.
-'''
+"""
 
 import argparse
 import os
@@ -16,15 +16,25 @@ import pickle
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data_dir", type=str, required=True, help="the directory which stores the image tsvfiles and the text jsonl annotations"
+        "--data_dir",
+        type=str,
+        required=True,
+        help="the directory which stores the image tsvfiles and the text jsonl annotations",
     )
     parser.add_argument(
-        "--splits", type=str, required=True, help="specify the dataset splits which this script processes, concatenated by comma \
-            (e.g. train,valid,test)"
+        "--splits",
+        type=str,
+        required=True,
+        help="specify the dataset splits which this script processes, concatenated by comma \
+            (e.g. train,valid,test)",
     )
+    # 这个参数一般不用填
     parser.add_argument(
-        "--lmdb_dir", type=str, default=None, help="specify the directory which stores the output lmdb files. \
-            If set to None, the lmdb_dir will be set to {args.data_dir}/lmdb"
+        "--lmdb_dir",
+        type=str,
+        default=None,
+        help="specify the directory which stores the output lmdb files. \
+            If set to None, the lmdb_dir will be set to {args.data_dir}/lmdb",
     )
     return parser.parse_args()
 
@@ -40,58 +50,69 @@ if __name__ == "__main__":
     # build LMDB data files
     if args.lmdb_dir is None:
         args.lmdb_dir = os.path.join(args.data_dir, "lmdb")
+    # 按 train, valid, test 分别建立 lmdb 文件夹
     for split in specified_splits:
         # open new LMDB files
         lmdb_split_dir = os.path.join(args.lmdb_dir, split)
         if os.path.isdir(lmdb_split_dir):
             print("We will overwrite an existing LMDB file {}".format(lmdb_split_dir))
         os.makedirs(lmdb_split_dir, exist_ok=True)
+
+        pairs_annotation_path = os.path.join(args.data_dir, "{}_texts.jsonl".format(split))
+        base64_path = os.path.join(args.data_dir, "{}_imgs.tsv".format(split))
+        pairs_size = os.path.getsize(pairs_annotation_path)
+        base64_size = os.path.getsize(base64_path)
+
+        # map_size 有点坑, 这会非常浪费空间的. 所以根据 getsize 来计算 map_size
         lmdb_img = os.path.join(lmdb_split_dir, "imgs")
-        env_img = lmdb.open(lmdb_img, map_size=1024**4)
+        env_img = lmdb.open(lmdb_img, map_size=int(base64_size * 1.5))  # 1.5 倍数可能也有点浪费
         txn_img = env_img.begin(write=True)
         lmdb_pairs = os.path.join(lmdb_split_dir, "pairs")
-        env_pairs = lmdb.open(lmdb_pairs, map_size=1024**4)
+        env_pairs = lmdb.open(lmdb_pairs, map_size=int(pairs_size * 2))  # 这个是纯文本, 倍数大点也没关系
         txn_pairs = env_pairs.begin(write=True)
 
         # write LMDB file storing (image_id, text_id, text) pairs
-        pairs_annotation_path = os.path.join(args.data_dir, "{}_texts.jsonl".format(split))
         with open(pairs_annotation_path, "r", encoding="utf-8") as fin_pairs:
             write_idx = 0
             for line in tqdm(fin_pairs):
                 line = line.strip()
                 obj = json.loads(line)
                 for field in ("text_id", "text", "image_ids"):
-                    assert field in obj, "Field {} does not exist in line {}. \
+                    assert (
+                        field in obj
+                    ), "Field {} does not exist in line {}. \
                         Please check the integrity of the text annotation Jsonl file."
                 for image_id in obj["image_ids"]:
-                    dump = pickle.dumps((image_id, obj['text_id'], obj['text'])) # encoded (image_id, text_id, text)
-                    txn_pairs.put(key="{}".format(write_idx).encode('utf-8'), value=dump)  
+                    # 这边是用 pickle 来序列化的, 所以加载的时候也要用 pickle 反序列化
+                    dump = pickle.dumps((image_id, obj["text_id"], obj["text"]))  # encoded (image_id, text_id, text)
+                    # 总之 key 和 value 都是 bytes 类型
+                    # key 是 write_idx, value 是 pickle 编码的 (image_id, text_id, text)
+                    txn_pairs.put(key="{}".format(write_idx).encode("utf-8"), value=dump)
                     write_idx += 1
                     if write_idx % 5000 == 0:
                         txn_pairs.commit()
                         txn_pairs = env_pairs.begin(write=True)
-            txn_pairs.put(key=b'num_samples',
-                    value="{}".format(write_idx).encode('utf-8'))
+            txn_pairs.put(key=b"num_samples", value="{}".format(write_idx).encode("utf-8"))
             txn_pairs.commit()
             env_pairs.close()
         print("Finished serializing {} {} split pairs into {}.".format(write_idx, split, lmdb_pairs))
 
+        # 写入图片
         # write LMDB file storing image base64 strings
-        base64_path = os.path.join(args.data_dir, "{}_imgs.tsv".format(split))
         with open(base64_path, "r", encoding="utf-8") as fin_imgs:
             write_idx = 0
             for line in tqdm(fin_imgs):
                 line = line.strip()
                 image_id, b64 = line.split("\t")
-                txn_img.put(key="{}".format(image_id).encode('utf-8'), value=b64.encode("utf-8"))
+                # key 是 image_id, value 是 base64 编码的图片
+                txn_img.put(key="{}".format(image_id).encode("utf-8"), value=b64.encode("utf-8"))
                 write_idx += 1
                 if write_idx % 1000 == 0:
                     txn_img.commit()
                     txn_img = env_img.begin(write=True)
-            txn_img.put(key=b'num_images',
-                    value="{}".format(write_idx).encode('utf-8'))
+            txn_img.put(key=b"num_images", value="{}".format(write_idx).encode("utf-8"))
             txn_img.commit()
-            env_img.close()                
+            env_img.close()
         print("Finished serializing {} {} split images into {}.".format(write_idx, split, lmdb_img))
 
     print("done!")
